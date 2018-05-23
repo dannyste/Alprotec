dbe5002d91d59778fc13a473d575161cdaf41c30
Danny Ponce <dponce@kruger.com.ec>
2018-05-22 22:56:06 -0500

    cambios e imagenes.

--- Alprotec/Presentacion/FrmEquipos.Designer.cs
+++ Alprotec/Presentacion/FrmEquipos.Designer.cs
@@ -28,44 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtBuscar = new System.Windows.Forms.TextBox();
-            this.lbBuscar = new System.Windows.Forms.Label();
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.lbCliente = new System.Windows.Forms.Label();
             this.dgvEquipos = new System.Windows.Forms.DataGridView();
             this.gbCriteriosBusqueda = new System.Windows.Forms.GroupBox();
-            this.rbCodigoInterno = new System.Windows.Forms.RadioButton();
-            this.rbClaseMaquina = new System.Windows.Forms.RadioButton();
-            this.rbCliente = new System.Windows.Forms.RadioButton();
+            this.cbUd = new System.Windows.Forms.ComboBox();
+            this.lbUnidadMedida = new System.Windows.Forms.Label();
+            this.cbMarca = new System.Windows.Forms.ComboBox();
+            this.lbMarca = new System.Windows.Forms.Label();
+            this.btnRestablecer = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.lbPotencia = new System.Windows.Forms.Label();
             this.pAcciones = new System.Windows.Forms.Panel();
             this.btnModificar = new System.Windows.Forms.Button();
             this.btnSalir = new System.Windows.Forms.Button();
             this.btnImprimir = new System.Windows.Forms.Button();
             this.btnEliminar = new System.Windows.Forms.Button();
             this.btnNuevo = new System.Windows.Forms.Button();
+            this.nudPotencia = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.dgvEquipos)).BeginInit();
             this.gbCriteriosBusqueda.SuspendLayout();
             this.pAcciones.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPotencia)).BeginInit();
             this.SuspendLayout();
             // 
-            // txtBuscar
+            // txtCliente
             // 
-            this.txtBuscar.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.txtBuscar.Font = new System.Drawing.Font("Cambria", 10F);
-            this.txtBuscar.Location = new System.Drawing.Point(33, 60);
-            this.txtBuscar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.txtBuscar.Name = "txtBuscar";
-            this.txtBuscar.Size = new System.Drawing.Size(355, 23);
-            this.txtBuscar.TabIndex = 21;
-            this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
+            this.txtCliente.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtCliente.Font = new System.Drawing.Font("Cambria", 10F);
+            this.txtCliente.Location = new System.Drawing.Point(32, 39);
+            this.txtCliente.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(355, 23);
+            this.txtCliente.TabIndex = 21;
             // 
-            // lbBuscar
+            // lbCliente
             // 
-            this.lbBuscar.AutoSize = true;
-            this.lbBuscar.Font = new System.Drawing.Font("Cambria", 10F);
-            this.lbBuscar.Location = new System.Drawing.Point(31, 42);
-            this.lbBuscar.Name = "lbBuscar";
-            this.lbBuscar.Size = new System.Drawing.Size(50, 16);
-            this.lbBuscar.TabIndex = 22;
-            this.lbBuscar.Text = "Buscar";
+            this.lbCliente.AutoSize = true;
+            this.lbCliente.Font = new System.Drawing.Font("Cambria", 10F);
+            this.lbCliente.Location = new System.Drawing.Point(30, 21);
+            this.lbCliente.Name = "lbCliente";
+            this.lbCliente.Size = new System.Drawing.Size(51, 16);
+            this.lbCliente.TabIndex = 22;
+            this.lbCliente.Text = "Cliente";
             // 
             // dgvEquipos
             // 
@@ -74,67 +79,112 @@
             this.dgvEquipos.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
             this.dgvEquipos.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dgvEquipos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvEquipos.Location = new System.Drawing.Point(22, 92);
+            this.dgvEquipos.Location = new System.Drawing.Point(22, 154);
             this.dgvEquipos.Name = "dgvEquipos";
             this.dgvEquipos.ReadOnly = true;
             this.dgvEquipos.RowHeadersVisible = false;
             this.dgvEquipos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvEquipos.Size = new System.Drawing.Size(420, 228);
+            this.dgvEquipos.Size = new System.Drawing.Size(420, 240);
             this.dgvEquipos.TabIndex = 30;
             this.dgvEquipos.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvEquipos_CellDoubleClick);
             // 
             // gbCriteriosBusqueda
             // 
-            this.gbCriteriosBusqueda.Controls.Add(this.rbCodigoInterno);
-            this.gbCriteriosBusqueda.Controls.Add(this.rbClaseMaquina);
-            this.gbCriteriosBusqueda.Controls.Add(this.rbCliente);
-            this.gbCriteriosBusqueda.Controls.Add(this.txtBuscar);
-            this.gbCriteriosBusqueda.Controls.Add(this.lbBuscar);
+            this.gbCriteriosBusqueda.Controls.Add(this.nudPotencia);
+            this.gbCriteriosBusqueda.Controls.Add(this.cbUd);
+            this.gbCriteriosBusqueda.Controls.Add(this.lbUnidadMedida);
+            this.gbCriteriosBusqueda.Controls.Add(this.cbMarca);
+            this.gbCriteriosBusqueda.Controls.Add(this.lbMarca);
+            this.gbCriteriosBusqueda.Controls.Add(this.btnRestablecer);
+            this.gbCriteriosBusqueda.Controls.Add(this.btnConsultar);
+            this.gbCriteriosBusqueda.Controls.Add(this.lbPotencia);
+            this.gbCriteriosBusqueda.Controls.Add(this.txtCliente);
+            this.gbCriteriosBusqueda.Controls.Add(this.lbCliente);
             this.gbCriteriosBusqueda.Font = new System.Drawing.Font("Cambria", 10F);
             this.gbCriteriosBusqueda.Location = new System.Drawing.Point(22, 3);
             this.gbCriteriosBusqueda.Name = "gbCriteriosBusqueda";
-            this.gbCriteriosBusqueda.Size = new System.Drawing.Size(420, 88);
+            this.gbCriteriosBusqueda.Size = new System.Drawing.Size(420, 145);
             this.gbCriteriosBusqueda.TabIndex = 31;
             this.gbCriteriosBusqueda.TabStop = false;
             this.gbCriteriosBusqueda.Text = "Criterios de búsqueda";
             // 
-            // rbCodigoInterno
+            // cbUd
             // 
-            this.rbCodigoInterno.AutoSize = true;
-            this.rbCodigoInterno.Font = new System.Drawing.Font("Cambria", 10F);
-            this.rbCodigoInterno.Location = new System.Drawing.Point(128, 17);
-            this.rbCodigoInterno.Name = "rbCodigoInterno";
-            this.rbCodigoInterno.Size = new System.Drawing.Size(115, 20);
-            this.rbCodigoInterno.TabIndex = 31;
-            this.rbCodigoInterno.Text = "Código interno";
-            this.rbCodigoInterno.UseVisualStyleBackColor = true;
-            this.rbCodigoInterno.CheckedChanged += new System.EventHandler(this.rb_CheckedChanged);
+            this.cbUd.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbUd.Font = new System.Drawing.Font("Cambria", 10F);
+            this.cbUd.FormattingEnabled = true;
+            this.cbUd.Items.AddRange(new object[] {
+            "Seleccione",
+            "HP",
+            "kW"});
+            this.cbUd.Location = new System.Drawing.Point(127, 87);
+            this.cbUd.Name = "cbUd";
+            this.cbUd.Size = new System.Drawing.Size(84, 23);
+            this.cbUd.TabIndex = 48;
             // 
-            // rbClaseMaquina
+            // lbUnidadMedida
             // 
-            this.rbClaseMaquina.AutoSize = true;
-            this.rbClaseMaquina.Font = new System.Drawing.Font("Cambria", 10F);
-            this.rbClaseMaquina.Location = new System.Drawing.Point(267, 17);
-            this.rbClaseMaquina.Name = "rbClaseMaquina";
-            this.rbClaseMaquina.Size = new System.Drawing.Size(133, 20);
-            this.rbClaseMaquina.TabIndex = 25;
-            this.rbClaseMaquina.Text = "Clase de máquina";
-            this.rbClaseMaquina.UseVisualStyleBackColor = true;
-            this.rbClaseMaquina.CheckedChanged += new System.EventHandler(this.rb_CheckedChanged);
+            this.lbUnidadMedida.AutoSize = true;
+            this.lbUnidadMedida.Font = new System.Drawing.Font("Cambria", 10F);
+            this.lbUnidadMedida.Location = new System.Drawing.Point(125, 69);
+            this.lbUnidadMedida.Name = "lbUnidadMedida";
+            this.lbUnidadMedida.Size = new System.Drawing.Size(27, 16);
+            this.lbUnidadMedida.TabIndex = 49;
+            this.lbUnidadMedida.Text = "ud.";
             // 
-            // rbCliente
+            // cbMarca
             // 
-            this.rbCliente.AutoSize = true;
-            this.rbCliente.Checked = true;
-            this.rbCliente.Font = new System.Drawing.Font("Cambria", 10F);
-            this.rbCliente.Location = new System.Drawing.Point(33, 17);
-            this.rbCliente.Name = "rbCliente";
-            this.rbCliente.Size = new System.Drawing.Size(69, 20);
-            this.rbCliente.TabIndex = 24;
-            this.rbCliente.TabStop = true;
-            this.rbCliente.Text = "Cliente";
-            this.rbCliente.UseVisualStyleBackColor = true;
-            this.rbCliente.CheckedChanged += new System.EventHandler(this.rb_CheckedChanged);
+            this.cbMarca.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbMarca.Font = new System.Drawing.Font("Cambria", 10F);
+            this.cbMarca.FormattingEnabled = true;
+            this.cbMarca.Location = new System.Drawing.Point(237, 87);
+            this.cbMarca.Name = "cbMarca";
+            this.cbMarca.Size = new System.Drawing.Size(150, 23);
+            this.cbMarca.TabIndex = 46;
+            // 
+            // lbMarca
+            // 
+            this.lbMarca.AutoSize = true;
+            this.lbMarca.Font = new System.Drawing.Font("Cambria", 10F);
+            this.lbMarca.Location = new System.Drawing.Point(235, 69);
+            this.lbMarca.Name = "lbMarca";
+            this.lbMarca.Size = new System.Drawing.Size(45, 16);
+            this.lbMarca.TabIndex = 47;
+            this.lbMarca.Text = "Marca";
+            // 
+            // btnRestablecer
+            // 
+            this.btnRestablecer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRestablecer.Font = new System.Drawing.Font("Cambria", 10F);
+            this.btnRestablecer.Location = new System.Drawing.Point(201, 116);
+            this.btnRestablecer.Name = "btnRestablecer";
+            this.btnRestablecer.Size = new System.Drawing.Size(90, 23);
+            this.btnRestablecer.TabIndex = 45;
+            this.btnRestablecer.Text = "Restablecer";
+            this.btnRestablecer.UseVisualStyleBackColor = true;
+            this.btnRestablecer.Click += new System.EventHandler(this.btnRestablecer_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnConsultar.Font = new System.Drawing.Font("Cambria", 10F);
+            this.btnConsultar.Location = new System.Drawing.Point(297, 116);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(90, 23);
+            this.btnConsultar.TabIndex = 44;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // lbPotencia
+            // 
+            this.lbPotencia.AutoSize = true;
+            this.lbPotencia.Font = new System.Drawing.Font("Cambria", 10F);
+            this.lbPotencia.Location = new System.Drawing.Point(30, 68);
+            this.lbPotencia.Name = "lbPotencia";
+            this.lbPotencia.Size = new System.Drawing.Size(60, 16);
+            this.lbPotencia.TabIndex = 33;
+            this.lbPotencia.Text = "Potencia";
             // 
             // pAcciones
             // 
@@ -144,7 +194,7 @@
             this.pAcciones.Controls.Add(this.btnEliminar);
             this.pAcciones.Controls.Add(this.btnNuevo);
             this.pAcciones.Font = new System.Drawing.Font("Cambria", 10F);
-            this.pAcciones.Location = new System.Drawing.Point(443, 11);
+            this.pAcciones.Location = new System.Drawing.Point(453, 49);
             this.pAcciones.Name = "pAcciones";
             this.pAcciones.Size = new System.Drawing.Size(70, 309);
             this.pAcciones.TabIndex = 32;
@@ -204,11 +254,25 @@
             this.btnNuevo.UseVisualStyleBackColor = false;
             this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
             // 
+            // nudPotencia
+            // 
+            this.nudPotencia.DecimalPlaces = 2;
+            this.nudPotencia.Location = new System.Drawing.Point(32, 87);
+            this.nudPotencia.Maximum = new decimal(new int[] {
+            50000,
+            0,
+            0,
+            0});
+            this.nudPotencia.Name = "nudPotencia";
+            this.nudPotencia.Size = new System.Drawing.Size(89, 23);
+            this.nudPotencia.TabIndex = 50;
+            this.nudPotencia.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
             // FrmEquipos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(522, 326);
+            this.ClientSize = new System.Drawing.Size(533, 403);
             this.Controls.Add(this.pAcciones);
             this.Controls.Add(this.dgvEquipos);
             this.Controls.Add(this.gbCriteriosBusqueda);
@@ -223,25 +287,31 @@
             this.gbCriteriosBusqueda.ResumeLayout(false);
             this.gbCriteriosBusqueda.PerformLayout();
             this.pAcciones.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nudPotencia)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtBuscar;
-        private System.Windows.Forms.Label lbBuscar;
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.Label lbCliente;
         private System.Windows.Forms.DataGridView dgvEquipos;
         private System.Windows.Forms.GroupBox gbCriteriosBusqueda;
-        private System.Windows.Forms.RadioButton rbCodigoInterno;
-        private System.Windows.Forms.RadioButton rbClaseMaquina;
-        private System.Windows.Forms.RadioButton rbCliente;
         private System.Windows.Forms.Panel pAcciones;
         private System.Windows.Forms.Button btnModificar;
         private System.Windows.Forms.Button btnSalir;
         private System.Windows.Forms.Button btnImprimir;
         private System.Windows.Forms.Button btnEliminar;
         private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Label lbPotencia;
+        private System.Windows.Forms.Button btnRestablecer;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.ComboBox cbUd;
+        private System.Windows.Forms.Label lbUnidadMedida;
+        private System.Windows.Forms.ComboBox cbMarca;
+        private System.Windows.Forms.Label lbMarca;
+        private System.Windows.Forms.NumericUpDown nudPotencia;
 
 
     }
